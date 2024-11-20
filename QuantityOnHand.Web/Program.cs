621eddde17ae7d79d48ed204b705c0c141eb1329
= <blake@modsi.io>
2024-11-19 23:46:54 -0600

    Delete account management and authentication Razor components.
    Removed account management pages such as ChangePassword, ConfirmEmail, and DeletePersonalData. Authentication-related Razor components like Auth, EnableAuthenticator, and Disable2fa were also deleted. These actions streamline the codebase by eliminating unused or deprecated components.

--- QuantityOnHand.Web/Program.cs
+++ QuantityOnHand.Web/Program.cs
@@ -1,11 +1,7 @@
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MudBlazor.Services;
 using QuantityOnHand.Components;
-using QuantityOnHand.Components.Account;
 using QuantityOnHand.Data;
-using QuantityOnHand.Data.Entities;
 using QuantityOnHand.Data.Repositories.HospitalItems;
 using QuantityOnHand.Data.Utilities;
 using QuantityOnHand.Domain.HospitalItem.Queries;
@@ -16,30 +12,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddCascadingAuthenticationState();
-builder.Services.AddScoped<IdentityUserAccessor>();
-builder.Services.AddScoped<IdentityRedirectManager>();
-builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
-
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultScheme = IdentityConstants.ApplicationScheme;
-        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-    })
-    .AddIdentityCookies();
-
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
                        throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString, b => b.MigrationsAssembly("QuantityOnHand.Data")));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddSignInManager()
-    .AddDefaultTokenProviders();
-
-builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
 builder.Services.AddMudServices();
 
@@ -57,7 +34,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error", true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -68,9 +44,6 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
-// Add additional endpoints required by the Identity /Account Razor components.
-app.MapAdditionalIdentityEndpoints();
 
 using (var scope = app.Services.CreateScope())
 {
